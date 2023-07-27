f191b81f90638ef9559ef0a392e21a7ac1932995
thn6666 <thn20030217@gmail.com>
2023-07-27 21:43:25 +0800

    new

---
+++ spiderxc/spiderxc/middlewares.py
@@ -0,0 +1,120 @@
+# Define here the models for your spider middleware
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+import random
+from scrapy import signals
+from spiderxc.settings import USER_AGENTS
+# useful for handling different item types with a single interface
+from itemadapter import is_item, ItemAdapter
+
+def get_cookies_dict():
+    cookies_str = 'JSESSIONID=EB4131126C9A9A1492BB36A9EF73C4B0; TS0171726e=01dde16a1e65da48985de9b6e07f0a47e67485e43eaf2f31338faeaa0f0c50ec72b0df05103415e7b5d9e86d0dcc487fd4ff639e98; _trs_uv=l9soc0sj_5296_k73l; _trs_ua_s_1=lke9yn5o_5305_1rob; TS01708977=01dde16a1e75b5f3e6afaa2d97b0d7a15f419accabe2df018175865df0a096a10dc077a1a5591629a4371b499a6bf022ea1583e7dd'
+    cookies_dict = {}
+    for item in cookies_str.split('; '):
+        key, value = item.split('=', maxsplit=1)
+        cookies_dict[key] = value
+        return cookies_dict
+COOKIES_DICT = get_cookies_dict()
+class SpiderxcSpiderMiddleware:
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the spider middleware does not modify the
+    # passed objects.
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_spider_input(self, response, spider):
+        # Called for each response that goes through the spider
+        # middleware and into the spider.
+
+        # Should return None or raise an exception.
+        return None
+
+    def process_spider_output(self, response, result, spider):
+        # Called with the results returned from the Spider, after
+        # it has processed the response.
+
+        # Must return an iterable of Request, or item objects.
+        for i in result:
+            yield i
+
+    def process_spider_exception(self, response, exception, spider):
+        # Called when a spider or process_spider_input() method
+        # (from other spider middleware) raises an exception.
+
+        # Should return either None or an iterable of Request or item objects.
+        pass
+
+    def process_start_requests(self, start_requests, spider):
+        # Called with the start requests of the spider, and works
+        # similarly to the process_spider_output() method, except
+        # that it doesn’t have a response associated.
+
+        # Must return only requests (not items).
+        for r in start_requests:
+            yield r
+
+    def spider_opened(self, spider):
+        spider.logger.info("Spider opened: %s" % spider.name)
+
+
+class SpiderxcDownloaderMiddleware:
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the downloader middleware does not modify the
+    # passed objects.
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_request(self, request, spider):
+        # Called for each request that goes through the downloader
+        # middleware.
+
+        # Must either:
+        # - return None: continue processing this request
+        # - or return a Response object
+        # - or return a Request object
+        # - or raise IgnoreRequest: process_exception() methods of
+        #   installed downloader middleware will be called
+        request.cookies = {
+
+        }
+        return None
+
+    def process_response(self, request, response, spider):
+        # Called with the response returned from the downloader.
+
+        # Must either;
+        # - return a Response object
+        # - return a Request object
+        # - or raise IgnoreRequest
+        request.cookies = COOKIES_DICT
+        return response
+
+    def process_exception(self, request, exception, spider):
+        # Called when a download handler or a process_request()
+        # (from other downloader middleware) raises an exception.
+
+        # Must either:
+        # - return None: continue processing this exception
+        # - return a Response object: stops process_exception() chain
+        # - return a Request object: stops process_exception() chain
+        pass
+
+    def spider_opened(self, spider):
+        spider.logger.info("Spider opened: %s" % spider.name)
+
+    class RandomUserAgentMiddleware:
+        def process_request(self, request, spider):
+            # 随机选择一个User-Agent并设置到请求头中
+            user_agent = random.choice(USER_AGENTS)
+            request.headers['User-Agent'] = user_agent
---
+++ spiderxc/spiderxc/spiders/baotou.py
@@ -0,0 +1,40 @@
+import scrapy
+from scrapy import Selector
+from scrapy import Request
+import copy
+from spiderxc.items import MovieItem
+class BaotouSpider(scrapy.Spider):
+    name = "baotou"
+    allowed_domains = ["nmj.baotou.gov.cn"]
+
+    def start_requests(self):
+        keywords = ['zwdt','qxdt']
+        for key in keywords:
+            for page in range(1,10):
+                url = f'http://nmj.baotou.gov.cn/{key}_{page}.jhtml'
+                yield scrapy.Request(url=url)
+
+    def parse(self, response):
+        sel = Selector(response)
+        list_items = response.xpath('//div[@class="scy_lbsj-right-nr"]/ul/li')
+
+        for list_item in list_items:
+            movie_item = MovieItem()
+            url = list_item.xpath('./a/@href').get()
+            movie_item['url'] = 'http://nmj.baotou.gov.cn' + url
+            movie_item['update_at'] = list_item.xpath('./span/text()').get()
+            movie_item['title'] = list_item.xpath('./a/text()').get()
+            detail_url = 'http://nmj.baotou.gov.cn' + url
+
+            yield Request(url=detail_url, callback=self.parse_detail, cb_kwargs={'item': movie_item})
+
+
+    def parse_detail(self, response, **kwargs):
+        sel = Selector(response)
+        movie_item = copy.deepcopy(kwargs['item'])
+        list_items = response.xpath('//*[@id="content_body"]')
+        for list_item in list_items:
+            data = list_item.xpath('./div[2]//text()').getall()
+            cleaned_data = ''.join([text.replace('\u3000', '').replace('\n', '').replace('\xa0', '').strip() for text in data])
+            movie_item['data'] = cleaned_data
+            yield movie_item
